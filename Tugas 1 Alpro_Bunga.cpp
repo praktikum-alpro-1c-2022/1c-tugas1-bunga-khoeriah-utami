ce36b6215e93948e8a37f292c4564c797cb9a807
Bungakhoeriah <114651565+Bungakhoeriah@users.noreply.github.com>
2022-10-28 07:34:06 +0700

    Tugas 1 Praktikum Alpo

---
+++ Tugas 1 Alpro_Bunga.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    long long npm = 2210631170113;
+    string namalengkap = "Bunga Khoeriah Utami";
+    int umur = 18;
+    string kelas = "1C Informatika";
+    string hobi = "Memasak dan Olahraga";
+    string goals = "Bahagiakan diri sendiri";
+    cout << "NPM : " << npm << endl;
+    cout << "Nama Lengkap : " << namalengkap << endl;
+    cout << "Umur : " << umur << endl;
+    cout << "Kelas : " << kelas << endl;
+    cout << "Hobi : " << hobi << endl;
+    cout << "Goals : " << goals << endl;
+    return 0;
+
+}
